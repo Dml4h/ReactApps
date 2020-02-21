c2042a41b466c4a3b1601898d6bdeafe1c28da4e
Dml4h <dml4h@mtmail.mtsu.edu>
2020-02-21 15:35:34 -0600

    added components

--- simple-app/src/App.js
+++ simple-app/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assests/colors.jpg";
+import HelloWorld from "./components/Hello";
+import ActionButton from "./components/ActionButtion";
+import {Layout} from "./components/Layout";
+import "./styles/App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HelloWorld />
+        <ActionButton />
+        <Layout />
       </header>
     </div>
   );
 }
-
-export default App;
+//ReactDOM.render(<Hello/>, document.getElementById('root'));
