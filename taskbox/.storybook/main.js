537ecf0808f792ac0b4406c7536ddf5db096692a
Devon Layton <dml4h@mtmail.mtsu.edu>
2020-05-23 13:17:06 -0500

    removed taskbox from uploader branch

--- taskbox/.storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs',
-    '@storybook/addon-links',
-  ],
-};
